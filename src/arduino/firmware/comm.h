ee2b75be41f2812e247b59d93d8c986b4e9532ea
Alex Cordonnier <alex@ajcordsmbp.local>
2014-08-10 16:44:29 -0400

    Added license info to each source file

--- src/arduino/firmware/comm.h
+++ src/arduino/firmware/comm.h
@@ -5,7 +5,23 @@
  * This file contains the external defines and prototypes for communicating
  * with the calculator and the serial port.
  *
- * Last modified July 13, 2014
+ * Last modified August 10, 2014
+ *
+ *
+ * Copyright (C) 2014  Alex Cordonnier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef COMM_H
